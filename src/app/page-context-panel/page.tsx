07b20a06648cd0ac842f90e5740d46239250a7c5
mjv21 <mvandermolen@gmail.com>
2026-02-24 10:36:30 -0500

    Add PagesContextPanel component with checklist

---
+++ src/app/page-context-panel/page.tsx
@@ -0,0 +1,11 @@
+tsx'use client';
+
+import ContentReadinessChecklist from '../../../components/ContentReadinessChecklist';
+
+export default function PagesContextPanel() {
+  return (
+    <main>
+      <ContentReadinessChecklist />
+    </main>
+  );
+}
