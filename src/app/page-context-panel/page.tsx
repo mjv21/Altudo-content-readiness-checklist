56ae36586972eb942d4dccc7566c9d582d6d477d
mjv21 <mvandermolen@gmail.com>
2026-02-24 10:59:47 -0500

    Delete PagesContextPanel component
    Remove PagesContextPanel component and its dependencies.

--- src/app/page-context-panel/page.tsx
+++ src/app/page-context-panel/page.tsx
@@ -1,11 +1,1 @@
-'use client';
 
-import ContentReadinessChecklist from '../../../components/ContentReadinessChecklist';
-
-export default function PagesContextPanel() {
-  return (
-    <main>
-      <ContentReadinessChecklist />
-    </main>
-  );
-}
