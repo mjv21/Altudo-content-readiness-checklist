9405a2feffa9e90d26d6abadeffafc7aa8b5b7b1
mjv21 <mvandermolen@gmail.com>
2026-02-24 11:33:06 -0500

    Implement headers for Content Security Policy
    Add Content Security Policy headers to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+ async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.sitecorecloud.io https://*.sitecore.com https://*.sitecorecontenthub.cloud",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
